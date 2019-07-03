b9818d8f511de6855958dae993f54c620e21ff3c
Lockedsher <664179701@qq.com>
2019-07-03 09:53:03 +0800

    big change

--- src/main/java/h5EDULive/service/impl/UserServiceImpl.java
+++ src/main/java/h5EDULive/service/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import javax.transaction.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
